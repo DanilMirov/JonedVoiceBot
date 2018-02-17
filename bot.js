d31aa5c4fd5ab5190574b082ddd924062d9544d2
zziger <zziger07@gmail.com>
2018-02-17 10:39:36 +0200

    upd nahoj

--- bot.js
+++ bot.js
@@ -11,7 +11,13 @@ client.on("guildMemberAdd", member => {
   member.send({embed});
 });
 client.on("message", message => {
-	if (message.content == '.test_greetings') {
+	if(message.author.bot) return;
+	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  	const command = args.shift().toLowerCase();
+
+	if (command == 'test_greetings') {
 	  const embed = new Discord.RichEmbed()
 	  .setColor(65280)
 	  .setDescription("_Добро пожаловать!_\nПриветсвуем тебя,наш новый участник!\nНадеемся, что тебе у нас понравится.\n\nТы попал на сервер __#JonedVoice__\nСервер, где находят друзей, играют с ними, да и просто проводят свободное время.\n\nДля того чтобы ты не запутался, администрация составила список каналов, которые ты должен обязательно\nпосетить, ибо там находится полезная информация для новичков, вроде тебя.\nИ так, перечень важных каналов:\n\nКанал **#info** - в основном предназначен для новичков сервера. Здесь находятся все ответы на твои вопросы,связаные с нашим сервером.\n\nВ разделе **#News** - ты можешь прочитать информацию об обновлениях нашего сервера и посмотреть на приятные плюшки.\n\nЕсли ты хочешь проверить свою удачу, то тебе в канал **#Contestchannel** - здесь ты можешь поучаствовать в конкурсе на внутрисерверную валюту и не только.\n\nТекстовый канал **#Chatik** - предназначен для общения между игроками.\n\nДругой текстовый канал, именуемый **#commands** - используется для команд ботов, которые присуствуют на сервере.\n\n**#fapchannel** - канал для клубнички.")
@@ -20,6 +26,12 @@ client.on("message", message => {
 	  .setTimestamp()
 	  message.author.send({embed});
 	}
+
+	if(command === "say" && message.author.id = "168255014282854401") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  	}
 });
 
 client.login(process.env.BOT_TOKEN);
