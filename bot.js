831dc0a3f6833ba18eb3441ae354c02120827f6a
zziger <zziger07@gmail.com>
2018-02-17 11:48:02 +0200

    ReferenceError: lol is not defined

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on("message", message => {
 	if(command === "say" && message.author.id == "168255014282854401" || message.author.id == "207821802431315968") {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage, lol);
+    message.channel.send(sayMessage);
   	}
 
   	if(command === "ayy") {
