7cc074e6ed2e1866bf7d2380d0a9f024996d55ac
zziger <zziger07@gmail.com>
2018-02-17 12:59:08 +0200

    аодлоладв

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on("message", message => {
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage).catch(O_o=>{message.reply('ты ебобо?');});
   	} else if (command === "clear") {
-  		if(!message.member.roles.some(r=>[rule.st_moder, rule.ml_admin, rule.st_admin, rule.creator].includes(r.id)) or message.member.id != '168255014282854401')
+  		if(!message.member.roles.some(r=>[rule.st_moder, rule.ml_admin, rule.st_admin, rule.creator].includes(r.id)))
   			return message.reply("Извините, ебобобам слово не давали!");
 		message.delete();
 		let content = message.content.slice(process.env.PREFIX.length + 8);
